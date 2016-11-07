77ba65161212bc7e80bcc2e5b58e2607c1819dd8
Rashid Awil <rashidawil437@gmail.com>
2016-11-07 13:28:36 +0000

    first commit

---
+++ public/src/js/index.jsx
@@ -0,0 +1,19 @@
+var React    = require('react');
+var ReactDOM = require('react-dom');
+var Router   = require('react-router').Router
+var Route    = require('react-router').Route
+var IndexRoute    = require('react-router').IndexRoute
+var Link     = require('react-router').Link
+var browserHistory = require('react-router').browserHistory
+
+
+/***********************
+********components******
+*************************/
+
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+  
+  </Router>
+), document.getElementById('app'))
